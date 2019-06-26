87e4d45017ecc2e507c8d1cf7e01dc6f99aa6d31
Salvatore Tosti <saltosti@gmail.com>
2019-06-26 15:25:24 -0700

    initial commit

---
+++ src/game/core.clj
@@ -0,0 +1,25 @@
+(ns tetris-clj.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m])
+  (:gen-class))
+
+(defn setup []
+  (q/background 0)
+  (q/frame-rate 30)
+  {:game {}})
+
+(defn draw [state]
+  state)
+
+(defn process-input [state key-information]
+   (println (:key key-information)))
+
+(defn -main []
+  (q/defsketch game-sketch
+    :title "gloom"
+    :size (let [[width height] [512 512]]
+            [width height])
+    :setup setup
+    :draw draw
+    :key-pressed process-input
+    :middleware [m/fun-mode]))
