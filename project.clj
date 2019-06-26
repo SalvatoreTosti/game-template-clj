cdb603772861719a0b98e867b9922919ab9ac954
Salvatore Tosti <saltosti@gmail.com>
2019-06-26 15:31:30 -0700

    Updates project to indicate MIT license

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject game-template-clj "0.1.0-SNAPSHOT"
   :description "Clojure game project template"
   :url "https://github.com/SalvatoreTosti/game-template-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "3.0.0"]]
   :main ^:skip-aot game.core
